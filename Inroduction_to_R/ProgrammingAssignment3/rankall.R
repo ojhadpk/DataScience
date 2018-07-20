323c167c88d041d7403e7dd46f3f8db344b58c59
ojhadpk <38914447+ojhadpk@users.noreply.github.com>
2018-07-20 18:07:19 +0530

    Completed changes in rankall.R

--- Inroduction_to_R/ProgrammingAssignment3/rankall.R
+++ Inroduction_to_R/ProgrammingAssignment3/rankall.R
@@ -1,32 +1,59 @@
+
 rankall <- function(outcome, num = "best") {
+        # Read input CSV file
         outcome_dt <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        # Define a list with all possible outcomes with respective column number
         outcomes_pos <- list("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+        
+        # Define a list with all unique states
         state.lst <- unique(outcome_dt$State)
+        
+        # Validity for outcome
         if ((outcome %in% names(outcomes_pos)) == F) {
                 stop(" invalid outcome ")
         }
-        #hospital_subset <- subset(outcome_dt, select = c("Hospital.Name", "State" 
-        #                                                 colnames(outcome_dt)[outcomes_pos[[outcome]]]))
-        #hospital_subset[,3] <- as.numeric(hospital_subset[,3])
-        #hospital_subset <- hospital_subset[complete.cases(hospital_subset), ]
         
-        for (state in state.lst) {
+        # Define list to store Hospital name and their state
+        Hospital.lst <- list()
+        state.out <- list()
+        
+        for (state in sort(state.lst)) {
+                
+                # Subset of data based on state 
                 state_subset <- subset(outcome_dt, State == state, 
                                        select = c("Hospital.Name", "State", colnames(outcome_dt)[outcomes_pos[[outcome]]]))
+                
+                # Convert mortality rate to numeric class
                 state_subset[, 3] <- as.numeric(state_subset[, 3])
+                
+                # Remove NA values
                 state_subset <- state_subset[complete.cases(state_subset), ]
+                
+                # Order on basis of mortality rate
                 hospital_rank <- state_subset[with(state_subset, order(state_subset[,3], Hospital.Name)), ]
-                Hospital_Name <- data.frame()
+                
+                # Condition for input rank of hospital
                 if (num == "best") {
-                        Hospital.lst <- rbind(hospital_rank[1,1], hospital_rank[1,2])
+                        Hospital.lst <- append(Hospital.lst, hospital_rank[1,1])
+                        state.out <- append(state.out, state)
                 }
                 else if (num == "worst") {
-                        Hospital.lst <- rbind(hospital_rank[nrow(hospital_rank), 1], hospital_rank[nrow(hospital_rank), 2])
+                        Hospital.lst <- append(Hospital.lst, hospital_rank[nrow(hospital_rank), 1])
+                        state.out <- append(state.out, state)
                 }
                 else {
-                        Hospital.lst <- rbind(hospital_rank[num, 1], hospital_rank[num, 2]) 
+                        Hospital.lst <- append(Hospital.lst, hospital_rank[num, 1])
+                        state.out <- append(state.out, state)
                 }
         }
-        Hospital_Name <- rbind(Hospital_Name, Hospital.lst)
+        
+        # Insert hospital name and respective state in a dataframe
+        Hospital_Name <- do.call(rbind, Map(data.frame, hospital = Hospital.lst, state = state.out))
+        
+        # Remove NA values (if no hospital on particular rank)
+        Hospital_Name <- Hospital_Name[complete.cases(Hospital_Name), ]
+        
+        # Return Hospital_Name
         return(Hospital_Name)
 }
